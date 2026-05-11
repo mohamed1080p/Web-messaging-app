577c881735560e110793c81be0b7595c155e05e2
mohamed1080p <mohamed7elnasharty@gmail.com>
2026-05-11 23:20:03 +0300

    feat: add "GetContacts" slice

---
+++ Web-messaging-app/Featuers/Contacts/GetContacts/GetContactsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Web_messaging_app.Infrastructure.Persistence.PostgreSql;
+
+namespace Web_messaging_app.Featuers.Contacts.GetContacts;
+
+public class GetContactsQueryHandler(AppDbContext _dbContext, IHttpContextAccessor _httpContextAccessor) : IRequestHandler<GetContactsQuery, List<ContactDto>>
+{
+    public async Task<List<ContactDto>> Handle(GetContactsQuery query, CancellationToken ct)
+    {
+        var ownerId = Guid.Parse(
+            _httpContextAccessor.HttpContext!.User
+                .FindFirstValue(ClaimTypes.NameIdentifier)!);
+
+        return await _dbContext.Contacts
+            .Where(c => c.OwnerId == ownerId)
+            .Select(c => new ContactDto(
+                c.ContactUser.Id,
+                c.ContactUser.UserName,
+                c.ContactUser.DisplayName,
+                c.ContactUser.AvatarUrl))
+            .ToListAsync(ct);
+    }
+}
